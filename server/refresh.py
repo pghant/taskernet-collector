de4d10709dc276d12f0f164c72d21793c1337620
Prasant Ghantasala <prasant.ghantasala@gmail.com>
2020-06-24 22:35:22 -0400

    Change refresh to use index sorted by recordUpdated

--- server/refresh.py
+++ server/refresh.py
@@ -13,7 +13,7 @@ db = TaskerNetDatabase()
 total_shares = db.record_count()
 
 # shares to run every 30 minutes to cover all in one week
-shares_to_run = total_shares // (24*7*2)
+shares_to_run = total_shares // (24*6*2)
 previous_week = int((dt.datetime.now() - dt.timedelta(days=7)).timestamp())
 
 records_to_update = db.records_modified_before(previous_week, shares_to_run)
